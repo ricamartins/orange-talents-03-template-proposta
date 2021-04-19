eb18677c7b7643801393f7579c8f52af05cc85ce
Ricardo Martins <martinsc.ricardo@gmail.com>
2021-04-19 10:20:43 -0300

    setup

---
+++ src/main/java/com/zup/microservice/ZupProposalApplication.java
@@ -0,0 +1,13 @@
+package com.zup.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupProposalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/microservice/ZupProposalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupProposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
